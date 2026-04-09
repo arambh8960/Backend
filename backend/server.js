dfa4fd7c9aa9eff7bdd6d9ae26df614ea184d731
Arambh Tiwari <arambh.tiwari11@gmail.com>
2026-04-09 19:08:49 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
