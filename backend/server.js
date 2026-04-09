7bdd5a1141d5faea953e32876d5a9f23f57a8f02
Arambh Tiwari <arambh.tiwari11@gmail.com>
2026-04-09 20:23:03 +0530

    How to connect frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,35 @@ dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+  {
+    id: 1,
+    title: "Dark Mode Lovers",
+    joke: "Why do programmers prefer dark mode? Because light attracts bugs!"
+  },
+  {
+    id: 2,
+    title: "Java vs C#",
+    joke: "Why do Java developers wear glasses? Because they don’t C#."
+  },
+  {
+    id: 3,
+    title: "Light Bulb Problem",
+    joke: "How many programmers does it take to change a light bulb? None. That’s a hardware problem."
+  },
+  {
+    id: 4,
+    title: "Broke Developer",
+    joke: "Why did the developer go broke? Because he used up all his cache."
+  },
+  {
+    id: 5,
+    title: "Debugging Truth",
+    joke: "Debugging is like being the detective in a crime movie where you are also the murderer."
+  }
+];
+    res.send(jokes);
 });
 
 const port = process.env.PORT || 3000;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+
+
+
+
+function App() {
+  const [jokes,setJokes]=useState([])
+  useEffect(()=>{
+    axios.get('api/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  },[])
+
+  return (
+    <>
+    <h1>Chai aur fullStack</h1>
+      <p>Jokes:{jokes.length}</p>
+      {
+        jokes.map((joke,index)=>(
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.joke}</p>
+
+          </div>
+
+        ))
+      }
+    </>
+      
+  )
+}
+
+export default App
